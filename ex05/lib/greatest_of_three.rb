94e2341f5db4e4f03e1112b9f69114bd890f0380
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-02-28 11:04:33 -0300

    Adding function to find the maximum among three numbers

--- ex05/lib/greatest_of_three.rb
+++ ex05/lib/greatest_of_three.rb
@@ -1,3 +1,11 @@
 def greatest_of_three(number1, number2, number3)
-
+  maior = number1
+  if number2 > maior
+    maior = number2
+  end
+  if number3 > maior
+    maior = number3 
+  end
+  return maior
 end
+
--- ex05/lib/main.rb
+++ ex05/lib/main.rb
@@ -1,12 +1,14 @@
 require_relative 'greatest_of_three'
 require_relative 'typing'
-
+puts "Vamos encontrar o maior número entre três números!"
 def main
   number1 = typing('1')
   number2 = typing('2')
   number3 = typing('3')
   result = greatest_of_three(number1, number2, number3)
-  puts "The greatest number is: #{result}"
+  puts "O maior número é: #{result}"
+
+  puts "Obrigado por usar nosso programa! "
 end
 
 main
