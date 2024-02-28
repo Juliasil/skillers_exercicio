41e9340ddd51146bc9c5d5034c474019137e750d
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-02-28 19:16:23 -0300

    Added function to determine if student was approved or not

--- ex06/lib/approval.rb
+++ ex06/lib/approval.rb
@@ -1,3 +1,8 @@
 def check_approval(grade)
-
+  result = grade
+  if grade >= 6
+    return  "Aprovado"
+  else grade < 6
+    return  "Reprovado"
+  end
 end
--- ex06/lib/main.rb
+++ ex06/lib/main.rb
@@ -2,9 +2,18 @@ require_relative 'approval'
 require_relative 'typing'
 
 def main
+  puts "Bem-vindo ao Verificador de Aprovação!"
+  puts "-------------------------------------------"
+  puts "Digite sua nota para ver se foi aprovado."
+  puts "Por favor, insira um valor entre 0 e 10."
+  puts "--------------------------------------------"
   grade = typing
   result = check_approval(grade)
+  puts "--------------------------------------------"
+  puts "Analisando sua nota..."
+  puts "--------------------------------------------"
   puts "Você foi #{result} com a nota #{grade}"
+  puts "--------------------------------------------"
 end
 
 main
