04c188214a3a45f863fd6aa58aaa40156635dfd7
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-03-01 10:47:09 -0300

    Added the ask_question function to check if the user's answer is correct

--- ex08/lib/main.rb
+++ ex08/lib/main.rb
@@ -1,10 +1,21 @@
+puts "---------------------------------------------"
+puts "Bem-vindo ao Jogo de Perguntas e Respostas!"
+puts "---------------------------------------------"
+puts "Neste jogo, você terá que responder perguntas sobre capitais de países."
+puts "Vamos começar!"
+puts "Digite sua resposta"
+
 require_relative 'question_game'
 require_relative 'typing'
 
+
 def main
   question = typing
+  user_answer = gets.chomp
   correct_answer = ['Tóquio','Brasília']
-  ask_question(question, correct_answer)
+  result = ask_question = ask_question(question, correct_answer, user_answer)
+  puts result ? 'Correto!' : 'Incorreto!'
+  puts "O jogo terminou. Obrigado por jogar!"
 end
 
 main
